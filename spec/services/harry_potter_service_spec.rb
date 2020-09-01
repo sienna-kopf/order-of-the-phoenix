614a89a016b9f0ed6e5011e3cde734c7db23d8e1
Sienna Kopf <princess.kopf@gmail.com>
2020-09-01 10:41:35 -0600

    Remove vcr blocks from services test

--- spec/services/harry_potter_service_spec.rb
+++ spec/services/harry_potter_service_spec.rb
@@ -4,17 +4,15 @@ describe HarryPotterService do
   context 'instance methods' do
     context '#search_members' do
       it "retruns members who are in the Order of the Phoenix" do
-        VCR.use_cassette('search_members_by_house') do
-          service = HarryPotterService.new
-          query = service.search_members("Gryffindor")
-          expect(query).to be_a Array
-          expect(query[0]).to be_an Hash
-          member_data = query[-1]
-          expect(member_data).to have_key :name
-          expect(member_data).to have_key :role
-          expect(member_data).to have_key :house
-          expect(member_data).to have_key :patronus
-        end
+        service = HarryPotterService.new
+        query = service.search_members("Gryffindor")
+        expect(query).to be_a Array
+        expect(query[0]).to be_an Hash
+        member_data = query[-1]
+        expect(member_data).to have_key :name
+        expect(member_data).to have_key :role
+        expect(member_data).to have_key :house
+        expect(member_data).to have_key :patronus
       end
     end
   end
